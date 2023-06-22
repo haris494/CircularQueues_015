e9c7df6c05a70f90140addf62435bda279b2bec6
haris494 <hariis.459@gmail.com>
2023-06-22 11:29:00 +0700

    cout << endl;

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -110,4 +110,7 @@ int main() {
 			cout << "2. Implement delete operation" << endl;
 			cout << "3. Display values" << endl;
 			cout << "4. exit" << endl;
-			cout << "Enter your choice (1-4): ;"
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl; 
+
